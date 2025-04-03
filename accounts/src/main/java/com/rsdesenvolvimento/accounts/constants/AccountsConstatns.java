ea46491b4849153d03a8b8cb78bcc5b0b437b118
RICKELMY SANTOS AZEVEDO DA CUNHA <rickelmy.cunha@trt8.jus.br>
2025-04-03 11:18:14 -0300

    feat: criação de classe para definir statuscode

---
+++ accounts/src/main/java/com/rsdesenvolvimento/accounts/constants/AccountsConstatns.java
@@ -0,0 +1,16 @@
+package com.rsdesenvolvimento.accounts.constants;
+
+public class AccountsConstatns {
+
+  private AccountsConstatns() {}
+
+  public static final String SAVINGS = "Savings";
+  public static final String ADDRESS = "123 Main Street, New York";
+  public static final String STATUS_201 = "201 Created";
+  public static final String MESSAGE_201 = "Account created successfully";
+  public static final String STATUS_200 = "200 OK";
+  public static final String MESSAGE_200 = "Request processed successfully";
+  public static final String STATUS_500 = "500 Internal Server Error";
+  public static final String MESSAGE_500 = "Internal server error";
+}
+
--- accounts/src/main/java/com/rsdesenvolvimento/accounts/controladores/AccountsController.java
+++ accounts/src/main/java/com/rsdesenvolvimento/accounts/controladores/AccountsController.java
@@ -1,9 +1,12 @@
 package com.rsdesenvolvimento.accounts.controladores;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/accounts")
 public class AccountsController {
+
 
 
 }
