e01ab57db58add0064df3d2db9b3b672787fff0b
RICKELMY SANTOS AZEVEDO DA CUNHA <rickelmy.cunha@trt8.jus.br>
2025-04-07 09:15:49 -0300

    feat: adição de dependencias ao pom

--- accounts/src/main/java/com/rsdesenvolvimento/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/rsdesenvolvimento/accounts/AccountsApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
 @SpringBootApplication(scanBasePackages = "com.rsdesenvolvimento.accounts")
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class AccountsApplication {
