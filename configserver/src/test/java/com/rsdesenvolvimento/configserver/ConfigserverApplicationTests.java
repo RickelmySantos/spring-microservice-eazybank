24813ee9beef1d75a5329f9e5bf067b122a528b4
¨Rickelmy <rickrickelmy97@gmail.com>
2025-04-22 21:21:27 -0300

    feat: configuração spring cloud

---
+++ configserver/src/test/java/com/rsdesenvolvimento/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsdesenvolvimento.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
